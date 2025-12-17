26dbdb883d0afa408cf12acda92ddf9bad465226
Saad Essonni <saad.essonni@epfl.ch>
2025-12-17 14:35:37 +0100

    Signaux Logiques: ajout de la classe AreaLogic,ajouts d'éléments liés dans ICMazeArea. Continuer

---
+++ icmaze/src/main/java/ch/epfl/cs107/icmaze/area/AreaLogic.java
@@ -0,0 +1,15 @@
+package ch.epfl.cs107.icmaze.area;
+
+import ch.epfl.cs107.play.signal.logic.LogicGate;
+
+public class AreaLogic extends LogicGate {
+    private boolean active = false;
+
+    @Override
+    public float getIntensity() {
+        return active ? 1.0f : 0.0f;
+    }
+    public void setActive(boolean state){
+        active = state;
+    }
+}
